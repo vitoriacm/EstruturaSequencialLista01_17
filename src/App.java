f6977c0ba2c2779e1dfab3e3f814f72b2d1e3b74
vitoriacm <anavitoria.cezar@gmail.com>
2023-05-05 13:51:03 -0300

    imprimindo quantidades

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
 
         double metragem;
 
-        System.out.print("Entre com a metragem a ser pintada em M2");
+        System.out.print("Entre com a metragem a ser pintada em M2: ");
         Scanner teclado = new Scanner(System.in);
         metragem = teclado.nextDouble();
         teclado.close();
@@ -27,6 +27,12 @@ public class App {
 
         
         double metragemMistura = metragem * 1.1;
+        double quantidadeLatasMistura = metragemMistura / (rendimentoLitros * volumeLatalitros);
+        double quantidadeLatasMisturaArrendondada = Math.ceil(metragemMistura /( rendimentoLitros * volumeLatalitros));
+        double quantidadeDesperdicioDaLata = quantidadeLatasMisturaArrendondada - quantidadeLatasMistura;
+        double quantidadeLitrosAConverterEmGaloes = (1 - quantidadeDesperdicioDaLata) * volumeLatalitros;
+        double quantidadeGaloesArrendondada = Math.ceil(quantidadeLitrosAConverterEmGaloes/ volumeDoGalao);
+        double quantidadeEmLataFinal = Math.floor(quantidadeLatasMistura);
 
         System.out.printf(" A metragem a ser pintade é de %f metros quadrados", metragem);
         System.out.println("");
@@ -35,7 +41,17 @@ public class App {
         System.out.printf("A quantidae de galões é %f e o valor total em galões é %f " , quantidadoGaloes, valorTotalGaloes);
         System.out.println("");
         System.out.printf(" A metragem para mistura é igual a %f", metragemMistura);
-
-
+        System.out.println("");
+        System.out.printf("A quantidade de latas para mistura é %f", quantidadeLatasMistura);
+        System.out.println("");
+        System.out.printf("A quantidade de latas para mistura arrendondada é %f", quantidadeLatasMisturaArrendondada);
+        System.out.println("");
+        System.out.printf("A quantidade desperdicio na lata  %f", quantidadeDesperdicioDaLata);
+        System.out.println("");
+        System.out.printf("A quantidade em litros a ser utilizada em galões é  %f", quantidadeLitrosAConverterEmGaloes);
+        System.out.println("");
+        System.out.printf("A quantidade arrendondada em galões é  %f", quantidadeGaloesArrendondada);
+        System.out.println("");
+        System.out.printf("A quantidade final em latas  é  %f e em galões %f", quantidadeEmLataFinal, quantidadeGaloesArrendondada);
     }
 }
